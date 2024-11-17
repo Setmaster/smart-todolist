98a893c339ce20ce1c74b5e6eb58e629e333375b
edgmsh <edgmsh@gmail.com>
2024-11-17 13:43:31 -0500

    users api added

--- routes/users-api.js
+++ routes/users-api.js
@@ -4,21 +4,82 @@
  *   these routes are mounted onto /api/users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
-
+const bcrypt = require("bcrypt");
 const express = require('express');
 const router  = express.Router();
 const userQueries = require('../db/queries/users');
 
-router.get('/', (req, res) => {
-  userQueries.getUsers()
-    .then(users => {
-      res.json({ users });
+// Create a new user
+router.post("/", (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+  database
+    .addUser(user)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "error" });
+      }
+
+      req.session.userId = user.id;
+      res.send("🤗");
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    .catch((e) => res.send(e));
+});
+
+// Log a user in
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  database.getUserWithEmail(email).then((user) => {
+    if (!user) {
+      return res.send({ error: "no user with that id" });
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.send({ error: "error" });
+    }
+
+    req.session.userId = user.id;
+    res.send({
+      user: {
+        name: user.name,
+        email: user.email,
+        id: user.id,
+      },
     });
+  });
+});
+
+// Log a user out
+router.post("/logout", (req, res) => {
+  req.session.userId = null;
+  res.send({});
+});
+
+// Return information about the current user (based on cookie value)
+router.get("/me", (req, res) => {
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.send({ message: "not logged in" });
+  }
+
+  database
+    .getUserWithId(userId)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "no user with that id" });
+      }
+
+      res.send({
+        user: {
+          name: user.name,
+          email: user.email,
+          id: userId,
+        },
+      });
+    })
+    .catch((e) => res.send(e));
 });
 
 module.exports = router;
