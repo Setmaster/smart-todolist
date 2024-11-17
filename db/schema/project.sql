d624f6e459a55ebd49a873a9aaf60b30d3930a06
edgmsh <edgmsh@gmail.com>
2024-11-17 09:07:27 -0500

    ERD and schema updated

---
+++ db/schema/project.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS todos CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  category_id int REFERENCES categories(id) ON DELETE CASCADE,
+  date_created timestamp NOT NULL SET DEFAULT now(),
+  todo_date timestamp NOT NULL,
+  complete_date timestamp,
+  enquiry text NOT NULL,
+  enquiry_processed boolean NOT NULL,
+  details text
+);
