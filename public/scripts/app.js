73dc208e3cd211d3b4a1e98027198480402e44b1
Andrew <andrewlang.p@gmail.com>
2024-11-19 18:10:41 -0500

    add modal to landing page and todo list edit page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,20 @@
 // Client facing scripts here
+document.addEventListener('DOMContentLoaded', function() {
+  const modal = document.getElementById('myModal');
+  const openModalBtn = document.getElementById('openModalBtn');
+  const closeModalBtn = document.querySelector('.login-modal-container .close');
+
+  openModalBtn.addEventListener('click', function() {
+      modal.style.display = 'block';
+  });
+
+  closeModalBtn.addEventListener('click', function() {
+      modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function(event) {
+      if (event.target === modal) {
+          modal.style.display = 'none';
+      }
+  });
+});
--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,17 +1,30 @@
 // Client facing scripts here
-$(() => {
-  $('#fetch-users').on('click', () => {
-    $.ajax({
-      method: 'GET',
-      url: '/api/users'
-    })
-    .done((response) => {
-      const $usersList = $('#users');
-      $usersList.empty();
 
-      for(const user of response.users) {
-        $(`<li class="user">`).text(user.name).appendTo($usersList);
-      }
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  const modal = document.getElementById('myModal');
+  const openModalBtn = document.getElementById('openModalBtn');
+  const closeModalBtn = document.querySelector('.login-modal-container .close');
+	const deleteBtn = document.querySelector('.fa-trash');
+
+  openModalBtn.addEventListener('click', function(e) {
+    // if (e.target.className.includes('edit')) {
+		// 	modal.style.display = 'block';
+		// 	return
+    // }
+		modal.style.display = 'block';
   });
+
+  closeModalBtn.addEventListener('click', function() {
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+			modal.style.display = 'none';
+    }
+  });
+
+	// deleteBtn.addEventListener('click', function() {
+
+	// })
 });
